cee446efee114bf2d3197103f3a350ea13fc396e
longjj <luojj26@mail2.sysu.edu.cn>
2016-10-23 14:22:14 +0800

    ready to write js

--- Puzzle/js/demo.js
+++ Puzzle/js/demo.js
@@ -1,3 +1,14 @@
 window.onload = function() {
+    // create the child images
+    var map = document.getElementById('map');
+    for (var i = 0; i < 15; ++i) {
+        var node = document.createElement("li");
+        map.appendChild(node);
+    }
+
+    resetMap();
+
+    document.getElementById('replay').addEventListener('click', resetMap);
+    document.getElementsByTagName('li').addEventListener('click', move);
 
 }
