18fed73ad36d0ad585f9a0f5307d6ccce1e13b14
longjj <luojj26@mail2.sysu.edu.cn>
2016-10-23 19:10:32 +0800

    initialize the js..

--- Puzzle/js/demo.js
+++ Puzzle/js/demo.js
@@ -1,7 +1,11 @@
 var images = ["../images/panda.jpg", "../images/totoro.png"];
 var count = 0;
 var mapURL = images[0];
-var isEmpty[16];
+
+const puzzlePos = [['0','0'],['25%','0'],['50%','0'],['75%', '0'],
+                    ['25%','0'],['25%','25%'],['25%','50%'],['25%','75%'],
+                    ['50%','0'],['50%','25%'],['50%','50%'],['50%','75%'],
+                    ['75%','0'],['75%','25%'],['75%','50%'],['75%','75%']];
 
 window.onload = function() {
     document.getElementById("nextPage").addEventListener("click", function() {
@@ -13,16 +17,12 @@ window.onload = function() {
 }
 
 function makeGame() {
-    for (var i = 0; i < 15; ++i) {
-        isEmpty[i] = false;
-    }
-    isEmpty[15] = true;
 
     var map = document.getElementById("map");
     while (map.firstChild) {
         map.removeChild(map.firstChild);
     }
-    for (var i = 0; i < 16; ++i) {
+    for (var i = 0; i < 15; ++i) {
         var puzzle = document.createElement("li");
         puzzle.setAttribute("id", ("img" + i));
         puzzle.style.backgroundImage = "url(" + mapURL + ")";
