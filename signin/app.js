17079e4d29048bb4c25f78712fc0a9792301f3c8
longjj <luojj26@mail2.sysu.edu.cn>
2016-12-20 15:03:07 +0800

    准备添加session 和 cookies 信息

--- signin/app.js
+++ signin/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require("./routes/index");
-var regist = require("./routes/regist");
 
 var app = express();
 
@@ -26,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // set the router here
 app.use('/', index);
-app.use('/regist', regist);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- signin/routes/index.js
+++ signin/routes/index.js
@@ -1,24 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-var user = {
-    userName: "temp",
-    userId: "temp",
-    phoneNum: "1111111",
-    email: "aaaa@qq.com"
-};
+var user = {};
 
-/* GET home page. */
+/* GET basic page. */
 router.get('/', function(req, res, next) {
     if (!isQueryUserInfo(req)) {
         res.render('signin');
     } else {
         var q_username = req.query.username;
         res.render('detail', {
-            username: q_username
+            userName: q_username
         });
     }
 });
+
+router.get("/regist", function(req, res) {
+    res.render("regist");
+});
+
+router.get("/detail", function(req, res) {
+    res.render("detail", user);
+});
+
+router.get("/forget", function(req, res) {
+    res.render("forget");
+});
+
+// 注册
+router.post('/regist', function(req, res, next) {
+    // begin to regist
+    user = req.body;
+    // console.log(user);
+    var formatFlag = userFormatJudger(user);
+    console.log("format judge result: " + formatFlag);
+    if (userFormatJudger(user)) {
+        res.redirect("/detail");
+    }
+});
+
+module.exports = router;
 
 function isQueryUserInfo(req) {
     var query = req.query;
@@ -29,12 +50,58 @@ function isQueryUserInfo(req) {
     }
 }
 
-router.get("/detail", function(req, res) {
-    res.render("detail", user);
-});
+function userFormatJudger(user) {
+    return userNameJudger(user.userName) && userIdJudger(user.userId) &&
+        passwdJudger(user.passwd) && repasswdJudger(user.passwd, user.repasswd) &&
+        phoneNumJudger(user.phoneNum) && emailJudger(user.email);
+};
 
-router.get("/forget", function(req, res) {
-    res.render("forget");
-});
+function userNameJudger(data) {
+    if (data.length < 6 || data.length > 18) {
+        return false;
+    }
+    var regex = /^[a-z]{1}[0-9_a-z]{2,11}$/
+    if (!regex.test(data)) {
+        return false;
+    }
+    return true;
+};
 
-module.exports = router;
+function userIdJudger(data) {
+    var regex = /[1-9]\d{7}/
+    if (!regex.test(data)) {
+        return false;
+    }
+    return true;
+};
+
+function passwdJudger(data) {
+    if (data.length > 12) {
+        return false;
+    }
+    var regex = /[0-9_a-zA-Z\-]{6,}/
+    if (!regex.test(data)) {
+        return false;
+    }
+    return true;
+};
+
+function repasswdJudger(passwd, data) {
+    return passwd === data;
+};
+
+function phoneNumJudger(data) {
+    var regex = /[1-9]\d{10}/
+    if (!regex.test(data)) {
+        return false
+    }
+    return true
+};
+
+function emailJudger(data) {
+    var regex = /^([\w-_]+(?:\.[\w-_]+)*)@((?:[a-z0-9]+(?:-[a-zA-Z0-9]+)*)+\.[a-z]{2,6})$/
+    if (!regex.test(data)) {
+        return false
+    }
+    return true
+};
